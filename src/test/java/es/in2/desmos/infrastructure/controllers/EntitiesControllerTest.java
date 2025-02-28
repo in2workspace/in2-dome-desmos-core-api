70fea267a95d851e2f1d496285f6c3af51b9e6b5
Albert Rodríguez <albert.rodriguez@in2.es>
2025-02-28 11:30:17 +0100

    fix(service): remove pep call

--- src/test/java/es/in2/desmos/infrastructure/controllers/EntitiesControllerTest.java
+++ src/test/java/es/in2/desmos/infrastructure/controllers/EntitiesControllerTest.java
@@ -2,13 +2,11 @@ package es.in2.desmos.infrastructure.controllers;
 
 import es.in2.desmos.domain.models.Entity;
 import es.in2.desmos.domain.services.broker.BrokerPublisherService;
-import es.in2.desmos.domain.services.policies.PepWebClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -16,8 +14,8 @@ import reactor.core.publisher.Mono;
 
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @WebFluxTest(EntitiesController.class)
@@ -26,9 +24,6 @@ class EntitiesControllerTest {
 
     @MockBean
     private BrokerPublisherService brokerPublisherService;
-
-    @MockBean
-    private PepWebClient pepWebClient;
 
     @Autowired
     private WebTestClient webTestClient;
@@ -44,9 +39,6 @@ class EntitiesControllerTest {
         when(brokerPublisherService.findEntitiesAndItsSubentitiesByIdInBase64(anyString(), any(), any()))
                 .thenReturn(Mono.just(expectedEntitiesList));
 
-        when(pepWebClient.doRequest(any(), any(), any(), any(), any()))
-                .thenReturn(Mono.empty());
-
         webTestClient
                 .get()
                 .uri("/api/v1/entities/{id}", id)
@@ -56,7 +48,5 @@ class EntitiesControllerTest {
                 .expectStatus().isOk()
                 .expectBodyList(Entity.class)
                 .isEqualTo(expectedEntitiesList);
-
-        verify(pepWebClient).doRequest("/api/v1/entities/urn%3Acatalog%3A1", HttpMethod.GET, null, null, "Bearer <token>");
     }
 }
